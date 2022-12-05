2a1e995c0ac4cc219af21be207bf27ef974377e2
David Nemec <david.nemec@rws.com>
2022-12-05 20:26:04 +0100

    Add core setup

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+// Heavily inspired (read as copy-pasted with minor adjustmens) by https://github.com/JanVargovsky/advent-of-code.
+
+var day = DateTime.UtcNow.Day;
+var dayFolder = $"Day{day:D2}";
+
+var solverType = Assembly.GetExecutingAssembly().GetType($"AdventOfCode2022.{dayFolder}.Solver") ?? throw new NotImplementedException("Solver not implemented.");
+dynamic solver = Activator.CreateInstance(solverType)!;
+
+var input = await GetOrDownloadInputAsync();
+var result = Convert.ToString(solver.Solve(input));
+Console.WriteLine(result);
+
+async Task<string> GetOrDownloadInputAsync()
+{
+    var path = Path.Combine(dayFolder, "input.txt");
+    if (!File.Exists(path))
+    {
+        const string AOCSESSION = "AOCSESSION";
+        var session = Environment.GetEnvironmentVariable(AOCSESSION, EnvironmentVariableTarget.User)
+            ?? throw new ArgumentException($"Env. variable '{AOCSESSION}' is missing.");
+        using HttpClient httpClient = new();
+        httpClient.DefaultRequestHeaders.Add("Cookie", $"session={session}");
+        var input = await httpClient.GetStringAsync($"https://adventofcode.com/2022/day/{day}/input");
+        Directory.CreateDirectory(Path.GetDirectoryName(path));
+        // downloaded input is using Unix line ending, so we convert according to current OS
+        await File.WriteAllTextAsync(path, string.Join(Environment.NewLine, input[..^1].Split("\n")));
+    }
+
+    return await File.ReadAllTextAsync(path);
+}
