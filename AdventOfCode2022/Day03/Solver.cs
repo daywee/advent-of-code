5ed74c25a1704e384654551c272f38f90b624676
David Nemec <david.nemec@rws.com>
2022-12-09 15:36:26 +0100

    Day03 part 1

---
+++ AdventOfCode2022/Day03/Solver.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode2022.Day03;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""") == 157);
+    }
+
+    public int Solve(string input)
+    {
+        var sum = 0;
+        var lines = input.Split(Environment.NewLine);
+        foreach (var line in lines)
+        {
+            var half = line.Length / 2;
+            var firstPart = line[..half].ToHashSet();
+            var secondPart = line[half..].ToHashSet();
+
+            var commonItem = firstPart.Intersect(secondPart).Single();
+            var priority = GetPriority(commonItem);
+
+            sum += priority;
+        }
+
+        return sum;
+    }
+
+    private int GetPriority(char c)
+    {
+        var alphabetLetterCount = 'z' - 'a' + 1;
+
+        int priority = char.ToLower(c) - 'a';
+
+        if (char.IsUpper(c))
+            priority += alphabetLetterCount;
+
+        return priority + 1;
+    }
+}
