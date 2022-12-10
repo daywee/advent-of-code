46b3846e6548a73f270880ad65519454434a9636
David Nemec <david.nemec@rws.com>
2022-12-10 11:33:46 +0100

    Day10 part 1

---
+++ AdventOfCode2022/Day10/Solver.cs
@@ -0,0 +1,196 @@
+﻿namespace AdventOfCode2022.Day10;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+        addx 15
+        addx -11
+        addx 6
+        addx -3
+        addx 5
+        addx -1
+        addx -8
+        addx 13
+        addx 4
+        noop
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx -35
+        addx 1
+        addx 24
+        addx -19
+        addx 1
+        addx 16
+        addx -11
+        noop
+        noop
+        addx 21
+        addx -15
+        noop
+        noop
+        addx -3
+        addx 9
+        addx 1
+        addx -3
+        addx 8
+        addx 1
+        addx 5
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx -36
+        noop
+        addx 1
+        addx 7
+        noop
+        noop
+        noop
+        addx 2
+        addx 6
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx 1
+        noop
+        noop
+        addx 7
+        addx 1
+        noop
+        addx -13
+        addx 13
+        addx 7
+        noop
+        addx 1
+        addx -33
+        noop
+        noop
+        noop
+        addx 2
+        noop
+        noop
+        noop
+        addx 8
+        noop
+        addx -1
+        addx 2
+        addx 1
+        noop
+        addx 17
+        addx -9
+        addx 1
+        addx 1
+        addx -3
+        addx 11
+        noop
+        noop
+        addx 1
+        noop
+        addx 1
+        noop
+        noop
+        addx -13
+        addx -19
+        addx 1
+        addx 3
+        addx 26
+        addx -30
+        addx 12
+        addx -1
+        addx 3
+        addx 1
+        noop
+        noop
+        noop
+        addx -9
+        addx 18
+        addx 1
+        addx 2
+        noop
+        noop
+        addx 9
+        noop
+        noop
+        noop
+        addx -1
+        addx 2
+        addx -37
+        addx 1
+        addx 3
+        noop
+        addx 15
+        addx -21
+        addx 22
+        addx -6
+        addx 1
+        noop
+        addx 2
+        addx 1
+        noop
+        addx -10
+        noop
+        noop
+        addx 20
+        addx 1
+        addx 2
+        addx 2
+        addx -6
+        addx -11
+        noop
+        noop
+        noop
+        """) == 13140);
+    }
+
+    public int Solve(string input)
+    {
+        var registerX = 1;
+        var cycle = 1;
+        var sum = 0;
+        var lines = input.Split(Environment.NewLine);
+        foreach (var line in lines)
+        {
+            var instruction = line.Split(' ');
+            switch (instruction[0])
+            {
+                case "noop":
+                    CycleCallback();
+                    cycle++;
+                    break;
+                case "addx":
+                    var parameter = int.Parse(instruction[1]);
+                    CycleCallback();
+                    cycle++;
+                    CycleCallback();
+                    cycle++;
+                    registerX += parameter;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        return sum;
+
+        void CycleCallback()
+        {
+            if ((cycle - 20) % 40 == 0)
+            {
+                var signalStrength = cycle * registerX;
+                sum += signalStrength;
+            }
+        }
+    }
+}
