f75630487fa08e18661936daecb2cf94a8add7a6
David Nemec <david.nemec@rws.com>
2023-12-01 12:37:14 +0100

    day 1 part 1

--- AdventOfCode2023/Day01/Solver.cs
+++ AdventOfCode2023/Day01/Solver.cs
@@ -5,14 +5,41 @@ internal class Solver
     public Solver()
     {
         Debug.Assert(Solve("""
-
-""") == "");
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+""") == 142);
     }
 
-    public string Solve(string input)
+    public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
-        var result = string.Empty;
+
+        var digits = new List<char>[rows.Length];
+
+        for (int i = 0; i < rows.Length; i++)
+        {
+            digits[i] = [];
+
+            var row = rows[i];
+
+            foreach (var c in row)
+            {
+                if (char.IsDigit(c))
+                {
+                    //var value = (int)char.GetNumericValue(c);
+                    digits[i].Add(c);
+                }
+            }
+        }
+
+        var result = digits
+            .Select(e => $"{e.First()}{e.Last()}")
+            .Select(e => int.Parse(e))
+            .Sum();
+
+
         return result;
     }
 }
