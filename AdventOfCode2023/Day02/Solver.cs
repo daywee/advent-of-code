254e0cd997605d10fd731d8f0d83cf45a48abb18
David Nemec <david.nemec@rws.com>
2023-12-02 12:28:59 +0100

    day 2 part 2

--- AdventOfCode2023/Day02/Solver.cs
+++ AdventOfCode2023/Day02/Solver.cs
@@ -10,7 +10,7 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-""") == 8);
+""") == 2286);
     }
 
     public int Solve(string input)
@@ -18,11 +18,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         var rows = input.Split(Environment.NewLine);
         var result = 0;
 
-        var bag = ParsePairs("12 red, 13 green, 14 blue");
-
         // TODO: try to use Span
         foreach (var row in rows)
         {
+            var bag = ParsePairs("0 red, 0 green, 0 blue");
+
             if (!(row.Split([':', ';']) is [var gamePart, .. var revealParts]))
                 throw new InvalidOperationException();
 
@@ -31,23 +31,30 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
             var reveals = revealParts.Select(ParsePairs).ToList();
 
-            if (reveals.All(e => Test(bag, e)))
-                result += gameNumber;
+            foreach (var reveal in reveals)
+            {
+                AdjustBag(bag, reveal);
+            }
+
+            var power = bag.Values.Aggregate((acc, curr) => acc * curr);
+
+            result += power;
         }
 
         return result;
     }
 
-    private static bool Test(Dictionary<string, int> bag, Dictionary<string, int> reveal)
+    private static void AdjustBag(Dictionary<string, int> bag, Dictionary<string, int> reveal)
     {
         foreach (var (color, neededCount) in reveal)
         {
             var availableCount = bag.GetValueOrDefault(color);
-            if (neededCount > availableCount)
-                return false;
-        }
 
-        return true;
+            if (neededCount > availableCount)
+            {
+                bag[color] = neededCount;
+            }
+        }
     }
 
     private static Dictionary<string, int> ParsePairs(string revealPart)
@@ -60,6 +67,4 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
         return cubePairs;
     }
-
-    private record CubePair(string Name, int Count);
 }
