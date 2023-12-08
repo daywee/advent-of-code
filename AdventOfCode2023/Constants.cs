e13db1783e62a5fe7faf7abc6ade03a5269ed33d
David Nemec <david.nemec@rws.com>
2023-12-08 09:07:58 +0100

    day 8 part 1

---
+++ AdventOfCode2023/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode2023;
+
+public static class Constants
+{
+    public const StringSplitOptions RemoveAndTrim = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+}
--- AdventOfCode2023/Day08/Solver.cs
+++ AdventOfCode2023/Day08/Solver.cs
@@ -5,14 +5,64 @@ internal class Solver
     public Solver()
     {
         Debug.Assert(Solve("""
+RL
 
-""") == "");
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+""") == 2);
     }
 
-    public string Solve(string input)
+    public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
-        var result = string.Empty;
-        return result;
+
+        var (instructions, nodes) = ParseInput(rows);
+
+        var startNode = "AAA";
+        var endNode = "ZZZ";
+
+        var currentNode = nodes[startNode];
+
+        var steps = 0;
+        do
+        {
+            var instruction = instructions[steps % instructions.Length];
+
+            steps++;
+            currentNode = instruction switch
+            {
+                'L' => nodes[currentNode.Left],
+                'R' => nodes[currentNode.Right],
+                _ => throw new InvalidOperationException(),
+            };
+        } while (currentNode.Value != endNode);
+
+        return steps;
     }
+
+    private (string Instructions, Dictionary<string, Node> Nodes) ParseInput(string[] rows)
+    {
+        var instructions = rows[0];
+        var nodes = new Dictionary<string, Node>();
+
+        foreach (var row in rows.Skip(2))
+        {
+            var rowParts = row.Split(new[] { '=', ',', '(', ')' }, Constants.RemoveAndTrim);
+
+            var currentNode = rowParts[0];
+            var leftNode = rowParts[1];
+            var rightNode = rowParts[2];
+
+            nodes[currentNode] = new Node(currentNode, leftNode, rightNode);
+        }
+
+        return (instructions, nodes);
+    }
+
+    private record Node(string Value, string Left, string Right);
 }
