53eb115557cb4faac5994be3e6d02964743b554e
David Nemec <david.nemec@rws.com>
2025-12-05 12:29:37 +0100

    create 2025 project

---
+++ AdventOfCode2025/Day10/Solver.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode.Year2025.Day10;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+
+""") == "");
+    }
+
+    public string Solve(string input)
+    {
+        var rows = input.Split(Environment.NewLine);
+        var result = string.Empty;
+        return result;
+    }
+}
---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using TextCopy;
+
+// Copied from github.com/JanVargovsky/advent-of-code
+
+const int year = 2025;
+var day = 1;
+
+// await CreateSolverStructureAsync("/Users/dnemec/workspace/advent-of-code/AdventOfCode2025");
+// return;
+
+var dayFolder = GetDayFolder(day);
+var solverType = Assembly.GetExecutingAssembly().GetType($"AdventOfCode.Year{year}.{dayFolder}.Solver")
+    ?? throw new ArgumentException("Solver not found.");
+dynamic solver = Activator.CreateInstance(solverType)!;
+
+var input = await GetOrDownloadInputAsync();
+var result = Convert.ToString(solver.Solve(input));
+Console.WriteLine(result);
+await ClipboardService.SetTextAsync(result);
+
+async Task<string> GetOrDownloadInputAsync()
+{
+    var path = Path.Combine(dayFolder, "input.txt");
+    if (!File.Exists(path))
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .Build();
+                
+        var session = config["aocSession"] ?? throw new ArgumentException("Key 'aocSession' is missing in User Secrets.");
+
+        using HttpClient httpClient = new();
+        httpClient.DefaultRequestHeaders.Add("Cookie", $"session={session}");
+        httpClient.DefaultRequestHeaders.UserAgent.Add(new("(github.com/daywee/advent-of-code by d4v1d.nemec@gmail.com)"));
+        var input = await httpClient.GetStringAsync($"https://adventofcode.com/{year}/day/{day}/input");
+        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+        // downloaded input is using Unix line ending, so we convert according to current OS
+        await File.WriteAllTextAsync(path, string.Join(Environment.NewLine, input[..^1].Split("\n")));
+    }
+
+    return await File.ReadAllTextAsync(path);
+}
+
+static string GetDayFolder(int day) => $"Day{day:D2}";
+
+static async Task CreateSolverStructureAsync(string projectPath)
+{
+    const string dayFolderVariable = "<DayFolder>";
+    string template = $$""""
+namespace AdventOfCode.Year{{year}}.{{dayFolderVariable}};
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+
+""") == "");
+    }
+
+    public string Solve(string input)
+    {
+        var rows = input.Split(Environment.NewLine);
+        var result = string.Empty;
+        return result;
+    }
+}
+
+"""";
+    const int from = 1;
+    const int to = 25;
+    for (int day = from; day <= to; day++)
+    {
+        var dayFolder = $"Day{day:D2}";
+        var directoryPath = Path.Combine(projectPath, dayFolder);
+        const string fileName = "Solver.cs";
+        var filePath = Path.Combine(directoryPath, fileName);
+
+        if (!File.Exists(filePath))
+        {
+            _ = Directory.CreateDirectory(directoryPath);
+            var content = template.Replace(dayFolderVariable, dayFolder);
+            Console.WriteLine($"Creating {day}");
+            await File.WriteAllTextAsync(filePath, content);
+        }
+    }
+}
